e8fe577c16ce59c7f3f61583987b65bc52708d2e
Theo Fernandes <theofcampp@gmail.com>
2025-01-20 19:27:24 -0300

    front-end da página de login e cadastro

---
+++ cadastro.php
@@ -0,0 +1,57 @@
+<!--<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="createAccount-login.css">
+    <title>Cadastro</title>
+</head>
+<body>
+    <div id="separation"></div>
+</body>
+</html>-->
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login-cadastro.css">
+    <title>Cadastro e Login</title>
+</head>
+<body>
+        <section id="login-section">
+            <h2>Login</h2>
+            <form>
+                <label for="login-username">NOME DE USUÁRIO</label>
+                <input type="text" id="login-username" name="username" required>
+                
+                <label for="login-email">EMAIL</label>
+                <input type="text" id="login-email" name="email" required>
+                
+                <label for="login-password">SENHA</label>
+                <input type="password" id="login-password" name="password" required>
+                
+                <button type="submit">Entrar</button>
+            </form>
+        </section>
+        <section id="register-section">
+            <h2>Cadastro</h2>
+            <form>
+                <label for="register-username">NOME DE USUÁRIO</label>
+                <input type="text" id="register-username" name="username" required>
+                
+                <label for="register-email">EMAIL</label>
+                <input type="email" id="register-email" name="email" required>
+                
+                <label for="register-password">SENHA</label>
+                <input type="password" id="register-password" name="password" required>
+                
+                <label for="confirm-password">CONFIRMAR SENHA</label>
+                <input type="password" id="confirm-password" name="confirm_password" required>
+                
+                <button type="submit">Cadastrar</button>
+            </form>
+        </section>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>StoraGG</title>
-    <link rel="stylesheet" href="style.css">
+    <title>PlayQueue</title>
+    <link rel="stylesheet" href="styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Montserrat:ital@0;1&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
@@ -12,8 +12,9 @@
 <body>
     <header>
         <div id="top-page-container">
-            <h1 id="head-intro" class="anton-regular">PlayQueue - Organize your past and future games</h1>
+            <h1 id="head-intro">PlayQueue - Organize your past and future games</h1>
         </div>
     </header>
+    <a href="cadastro.php" id="cadastro">Link para cadastro (temporário)</a>
 </body>
 </html>
