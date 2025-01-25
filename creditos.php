c97b4f6237ae06d0ddcdbc9297d7d0f216c58b79
AndréLuiz <161881256+AndreDecoRDC@users.noreply.github.com>
2025-01-25 19:10:14 -0300

    Adição de conteúdo na Home, criação de elementos na NavBar + correções

---
+++ creditos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="creditos.css">
+    <title>Créditos</title>
+</head>
+<body>
+    <header>
+        <a href="index.html"> <img src="imgs/arrow_back_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png" alt=""> </a>
+        <nav>
+            <div id="top-page-logo">
+                <h1 id="head-intro">PlayQueue</h1>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
