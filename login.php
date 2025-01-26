3769bd70c0bc14b8b8e69264277914226d61c882
AndréLuiz <161881256+AndreDecoRDC@users.noreply.github.com>
2025-01-25 22:01:20 -0300

    correção dos inputs na página de login e cadastro

--- login.php
+++ login.php
@@ -2,42 +2,7 @@
     $db = mysqli_connect("localhost", "root", "", "playqueue");
     $db->set_charset("utf8");
 
-<<<<<<< Updated upstream
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css">
-    <title>Login</title>
-</head>
-<body>
-    <header>
-        <a href="index.html"> <img src="imgs/arrow_back_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png" alt=""> </a>
-        <nav>
-            <div id="top-page-logo">
-                <h1 id="head-intro">PlayQueue</h1>
-            </div>
-        </nav>
-    </header>
-        <section id="login-section">
-            <h2>Login</h2>
-            <form>
-                <label for="register-username">NOME DE USUÁRIO</label>
-                <input type="text" id="register-username" name="username" required>
-                
-                <label for="register-email">EMAIL</label>
-                <input type="email" id="register-email" name="email" required>
-                
-                <label for="register-password">SENHA</label>
-                <input type="password" id="register-password" name="password" required>
-                
-                <button type="submit">Entrar</button>
-            </form>
-        </section>
-</body>
-</html>
-=======
+    
     if (!$db) {
         $description = "Erro: " . mysqli_connect_error(); 
 
