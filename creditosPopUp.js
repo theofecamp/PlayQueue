83247650b873f4232daa9ab9186a20e4e7f9bfe4
Theo Fernandes <theofcampp@gmail.com>
2025-01-27 00:21:59 -0300

    Criação de janela pop-up na página de créditos com css básico temporário

---
+++ creditosPopUp.js
@@ -0,0 +1,37 @@
+let andre = document.querySelector("#funcoes-andre")
+let theo = document.querySelector("#funcoes-theo")
+let wesley = document.querySelector("#funcoes-wesley")
+let vitoria = document.querySelector("#funcoes-vitoria")
+
+
+function showAndre() {
+    andre.style.display = 'inline-block'
+}
+
+function hideAndre() {
+    andre.style.display = 'none'
+}
+
+function showTheo() {
+    theo.style.display = 'inline-block'
+}
+
+function hideTheo() {
+    theo.style.display = 'none'
+}
+
+function showWesley() {
+    wesley.style.display = 'inline-block'
+}
+
+function hideWesley() {
+    wesley.style.display = 'none'
+}
+
+function showVitoria() {
+    vitoria.style.display = 'inline-block'
+}
+
+function hideVitoria() {
+    vitoria.style.display = 'none'
+}
